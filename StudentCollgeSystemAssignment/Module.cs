7d29e02dca37b2db2e98c566f204f6def9a8c65d
laxman.kolate@waiin.com <laxman.kolate@waiin.com>
2022-07-14 17:12:15 +0530

    Add project files.

---
+++ StudentCollgeSystemAssignment/Module.cs
@@ -0,0 +1,150 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Module : IModule
+{
+    List<ModuleInfo> modules = new List<ModuleInfo>();
+    string s = new string('-', 70);
+
+
+    public void AddModule(ModuleInfo moduleInfo)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+
+        modules.Add(moduleInfo);
+    }
+
+    public void AddModule()
+    {
+
+        Console.WriteLine("Enter Subject name : ");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("Enter Subject Description : ");
+        var description = Console.ReadLine();
+
+        Console.WriteLine("Enter Course Fees : ");
+        var courseFeesText = Console.ReadLine();
+
+        Console.WriteLine("Enter Startdate : ");
+        var startDateText = Console.ReadLine();
+
+        ModuleInfo module = new ModuleInfo(name, description, double.Parse(courseFeesText), DateTime.Parse(startDateText));
+        modules.Add(module);
+    }
+
+    public void DisplayAllModule()
+    {
+        Console.WriteLine(s);
+        Console.WriteLine("\t\tModule Listing Report");
+        Console.WriteLine(s);
+        Console.WriteLine("|Name           |Description              |Fees      |Start Date     |");
+        Console.WriteLine(s);
+
+        foreach (var module in modules)
+        {
+            Console.WriteLine($"|{module.Name,-15}|{module.Discription,-25}|{module.Fees,10:N}|{module.DateTime,-15:dd-MMM-yyyy}|");
+        }
+
+        Console.WriteLine(s);
+    }
+
+    public void DisplayAnyModule()
+    {
+        Console.WriteLine(s);
+        Console.Write("Enter module name to search : ");
+        string searchModule = Console.ReadLine();
+
+        foreach (var module in modules)
+        {
+            if (searchModule == module.Name)
+            {
+                module.DisplaySingleModule();
+            }
+        }
+        Console.WriteLine(s);
+
+    }
+
+    public void findStudentMarks() //Find student mark is not Working
+    {
+       Console.WriteLine(s);
+       var subjectSearchText = Console.ReadLine();
+        foreach(var subject in modules)
+        {
+            if (Student.name == subjectSearchText)
+            {
+                Console.WriteLine("Student Total Marks:" + Student.TotalMarks);
+                break;
+            }
+            else
+                Console.WriteLine("Wrong Inpute");
+        }
+        Console.WriteLine(s);
+
+    }
+
+
+
+    public void UpdateModule()
+    {
+        Console.WriteLine(s);
+        Console.WriteLine("Search name of the record you want to update : ");
+        string updateName = Console.ReadLine();
+
+        foreach (var module in modules)
+
+        {
+
+            if (updateName == module.Name)
+            {
+
+                Console.Write($"What you want to update in {updateName}'s record. \n 1. Subject Name \n 2.Description \n 3.Course \n 4.Start date");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter Subject name : ");
+                        var Sname = Console.ReadLine();
+                        module.moduleName = Sname;
+                        Console.WriteLine("Name updated successfully");
+                        break;
+
+                    case 2:
+                        Console.Write("Enter Description : ");
+                        var Sage = Console.ReadLine();
+                        module.moduleName = Sage;
+                        Console.WriteLine("Desciption updated successfully");
+                        break;
+
+                    case 3:
+                        Console.Write("Enter Course : ");
+                        var Sdob = Console.ReadLine();
+                        module.moduleName = Sdob;
+                        Console.WriteLine("Courese updated successfully");
+                        break;
+
+                    case 4:
+                        Console.Write("Enter Start date : ");
+                        var Sgender = Console.ReadLine();
+                        module.moduleName = Sgender;
+                        Console.WriteLine("Gender updated successfully");
+                        break;
+
+                    
+
+                    default:
+                        if (choice == 0)
+                            Environment.Exit(0);
+                        break;
+                }
+
+            }
+        }
+
+
+        Console.WriteLine(s);
+    }
+}
+
---
+++ StudentCollgeSystemAssignment/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Test1();
+
+void Test1()
+{
+    IModule module = new Module();
+    module.AddModule(new ModuleInfo("HTML", "GREAT", 2500, DateTime.Now));
+    module.AddModule(new ModuleInfo("PHP", "GREAT", 4500, DateTime.Now));
+    module.AddModule(new ModuleInfo("C#", "GREAT", 4500, DateTime.Now));
+
+    IStudent student = new Student();
+    student.addStudentRecord(new StudentInfo("laxman", 21, DateTime.Now, Gender.Male));
+    student.addStudentRecord(new StudentInfo("salim", 25, DateTime.Now, Gender.Male));
+
+    int choice = 0;
+    do
+    {
+        Console.WriteLine("** College Student Registration **");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("1. Add Student Record");
+        Console.WriteLine("2. Update Student Record");
+        Console.WriteLine("3. Get Any Student Record");
+        Console.WriteLine("4. Get All Student Record");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("5. Add Module");
+        Console.WriteLine("6. Get any module details");
+        Console.WriteLine("7. Get All module details");
+        Console.WriteLine("8. Update Module");
+        Console.WriteLine("9. Find std marks");
+        Console.WriteLine("0. Exit..");
+        Console.WriteLine("Enter your choice : ");
+        choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+
+            case 1:
+                student.addStudentRecord();
+                break;
+            case 2:
+                student.updateStudent();
+                break;
+            case 3:
+                student.displayAnyStudentRecord();
+                break;
+            case 4:
+                student.displayAllStudentRecord();
+                break;
+            case 5:
+                module.AddModule();
+                break;
+            case 6:
+                module.DisplayAnyModule();
+                break;
+            case 7:
+                module.DisplayAllModule();
+                break;
+            case 8:
+                module.UpdateModule();
+                break;
+            default:
+                if (choice == 0)
+                    Environment.Exit(0);
+                else
+                    Console.WriteLine("Wrong input");
+                break;
+        }
+    } while (choice != 0);
+}
